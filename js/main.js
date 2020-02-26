c1642d22b13fc585380a480110ff9b6e9b7fe455
iamfrank <frank@magenta.dk>
2020-02-26 18:41:19 +0100

    use deviceorientationevent

--- js/main.js
+++ js/main.js
@@ -1,45 +1,12 @@
-
-var gn = new GyroNorm();
-
-gn.init({
-    
-    frequency: 50,
-    orientationBase: GyroNorm.WORLD
-        
-}).then(function() {
-  gn.start(function(data){
-    // Process:
-    // data.do.alpha	( deviceorientation event alpha value )
-    // data.do.beta		( deviceorientation event beta value )
-    // data.do.gamma	( deviceorientation event gamma value )
-    // data.do.absolute	( deviceorientation event absolute value )
-
-    // data.dm.x		( devicemotion event acceleration x value )
-    // data.dm.y		( devicemotion event acceleration y value )
-    // data.dm.z		( devicemotion event acceleration z value )
-
-    // data.dm.gx		( devicemotion event accelerationIncludingGravity x value )
-    // data.dm.gy		( devicemotion event accelerationIncludingGravity y value )
-    // data.dm.gz		( devicemotion event accelerationIncludingGravity z value )
-
-    // data.dm.alpha	( devicemotion event rotationRate alpha value )
-    // data.dm.beta		( devicemotion event rotationRate beta value )
-    // data.dm.gamma	( devicemotion event rotationRate gamma value )
-    
-    var n = data.do.alpha;
-    showAlpha(n);
-    
-  });
-}).catch(function(e){
-    
-    $('.no-api').show();
-    
-});
+function orientationHandler(ev) {
+    requestAnimationFrame(showAlpha(ev.alpha))
+}
 
 function showAlpha(a) {
     $('#rose').attr('style', 'transform: rotate(' + a + 'deg)');
 }
 
+window.addEventListener('deviceorientation', orientationHandler, false)
 
 /*
  * Register service worker
@@ -47,13 +14,13 @@ function showAlpha(a) {
  */
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('../dist/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('../dist/sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
-  });
 }
